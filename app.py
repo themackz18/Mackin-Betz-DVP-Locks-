958e52752bebe8c0b9db42337c3a2a9fa13ec470
themackz18 <themackz18@gmail.com>
2026-04-11 11:33:17 -0700

    Update app.py

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def api_report():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port)
